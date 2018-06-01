83a41a1151e43ec7d870c64f7861c8e9ccc2ed01
Jose Bentancour <josebentancour@gmail.com>
2018-06-01 11:13:58 -0300

    Update main.c

--- main.c
+++ main.c
@@ -11,12 +11,15 @@
 #include "shell.h"
 #include "logger.h"
 
-#define IDLE_TICKS 120                  /* Ticks de timeout para ui (30 s)*/
+#define IDLE_TICKS 120                  /* Ticks de timeout (30 s). */
+uint8_t idle_timer;                     /* Variable para el timeout. */
 
+/* Variables para ble_uart. */
 ble_uart_status_t ble_uart_status;
 uint8_t ble_uart_rx_msg[MAX_LEN];
 uint8_t ble_uart_tx_msg[MAX_LEN];
 
+/* Flags */
 volatile uint8_t ble_uart_rx_flag;
 volatile uint8_t ble_uart_tx_flag;
 volatile uint8_t rtc_tick_flag;
@@ -25,24 +28,20 @@ volatile uint8_t gpio_up_flag;
 volatile uint8_t gpio_down_flag;
 volatile uint8_t logger_send_flag;
 
+/* Variables para shell. */
 ShellFunPtr quefuncion;
 unsigned int argc;
 char* argv[64];
-
-uint8_t tx_length;
-
 int retval;
 
-uint8_t idle_timer;
-
-/**@brief Application main function.
+/**@brief Main.
  */
 int main(void)
 {
     NRF_LOG_INIT(NULL);
     NRF_LOG_INFO("main init\r\n");
     
-    /* Initialize. */
+    /* Inicializacion de modulos y variables. */
     ble_uart_rx_set_flag(&ble_uart_rx_flag);
     ble_uart_tx_flag = 1;
     ble_uart_tx_set_flag(&ble_uart_tx_flag);
@@ -65,11 +64,12 @@ int main(void)
 
     idle_timer = 0;
     
-    /* Enter main loop. */
+    /* Loop principal. */
     for (;;)
     {
         NRF_LOG_FLUSH();
         
+        /* Se resiono el boton OK. */
         if (gpio_ok_flag)
         {
             idle_timer = 0;
@@ -77,10 +77,11 @@ int main(void)
             nrf_delay_ms(5);
             if(!gpio_ok_flag)
             {
-                ui_process_event(pressed_ok);
+                ui_process_event(pressed_ok); /* Se envia el evento a ui. */
             }
         }
         
+        /* Se resiono el boton UP. */
         if (gpio_up_flag)
         {
             idle_timer = 0;
@@ -88,10 +89,11 @@ int main(void)
             nrf_delay_ms(5);
             if(!gpio_up_flag)
             {
-                ui_process_event(pressed_up);
+                ui_process_event(pressed_up); /* Se envia el evento a ui. */
             }
         }
         
+        /* Se resiono el boton DOWN */
         if (gpio_down_flag)
         {
             idle_timer = 0;
@@ -99,10 +101,11 @@ int main(void)
             nrf_delay_ms(5);
             if(!gpio_down_flag)
             {
-                ui_process_event(pressed_down);
+                ui_process_event(pressed_down); /* Se envia el evento a ui. */
             }
         }
-              
+         
+        /* Se incremento el rtc, este evento es periodico. */
         if(rtc_tick_flag)
         {
             rtc_tick_flag = 0;
@@ -112,7 +115,7 @@ int main(void)
                 idle_timer = 0;
             }
             
-            ui_process_event(time_update);
+            ui_process_event(time_update); /* Se envia el evento a ui. */
             
             idle_timer += 1;
             if(idle_timer >= IDLE_TICKS)
@@ -122,17 +125,19 @@ int main(void)
             }                
         }
         
+        /* Se recibio un nuevo mensaje por ble_uart */
         if (ble_uart_rx_flag) 
         {
-            // Se recibe un valor nuevo por BLE UART
+            // Se recibe un valor nuevo por ble_uart
             uint16_t length = ble_uart_get_msg(ble_uart_rx_msg);
             ble_uart_rx_flag = 0;
             
-            //Se agrega delimitador NULL al final del buffer
+            //Se agrega delimitador NULL al final del buffer para convertirlo en string
             ble_uart_rx_msg[length - 1]='\0';
+            
             NRF_LOG_DEBUG("ble_uart_rx_msg %s\r\n", (uint32_t) ble_uart_rx_msg);
             
-            // Se revisa si el comando concuerda con alguno de los definidos en SHELL 
+            // Se revisa si el comando concuerda con alguno de los definidos en shell 
             switch (sisem_shell((char*)ble_uart_rx_msg,&quefuncion, &argc, argv))
             {
                 case OK:						
@@ -142,7 +147,7 @@ int main(void)
                     break;
                     
                 case EXIT:						
-                    // Terminar el programa de test
+                    // Terminar comunicacion bluetooth
                     ble_uart_disconnect();
                     ble_uart_advertising_stop();
                     NRF_LOG_DEBUG("sisem_shell Exit\r\n");
@@ -160,25 +165,21 @@ int main(void)
             }
         }
         
-        if (logger_send_flag)   //LOGGER_SEND_FLAG es la que comunica que logger tiene un valor pendiente para enviar
-        {
-            //NRF_LOG_DEBUG("main logger_send_flag\r\n");
-            //NRF_LOG_FLUSH();
-            
-            if (ble_uart_tx_flag) //Revisar si hay que considerar el caso del primer dato 
-            {
-                NRF_LOG_DEBUG("main ble_uart_tx_flag\r\n");
-                NRF_LOG_FLUSH();
-                
-                // Se llama a LOGGER_SEND para que ponga en BLE_UART_TX_MSG el mensaje que se quiere enviar y en TX_LENGTH el largo del archvio
+        /* Indica que logger tiene un valor pendiente para enviar */
+        if (logger_send_flag)
+        {           
+            if (ble_uart_tx_flag) // Revisar si ble_uart esta lito para enviar un nuevo mensaje 
+            {                
+                // Se llama a logger_send para que ponga en ble_uart_tx_msg el mensaje que se quiere enviar y en tx_length el largo del mensaje
+                uint8_t tx_length;
                 tx_length = logger_send(ble_uart_tx_msg);
                 
                 NRF_LOG_DEBUG("main tx_length %d\r\n", tx_length);
                 NRF_LOG_FLUSH();
                 
-                // Se le pasa a BLE_UART_DATA_SEND el puntero con el dato
+                // Se le pasa a ble_uart_data_send el puntero con el mensaje
                 ble_uart_data_send(ble_uart_tx_msg, tx_length);
-                // Una vez logre enviar todo entrar· denuevo a este condicional hasta que LOGGER_SEND considere que no hay m·s datos para enviar
+                // Una vez logre enviar todo entrar√° denuevo a este condicional hasta que logger_send considere que no hay m√°s datos para enviar
             }
         }
     }
